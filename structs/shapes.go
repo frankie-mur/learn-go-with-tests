8ce5897e831e77980c237758166f603a659cb890
Frankie <frankidatank@gmail.com>
2023-10-22 22:06:02 -0700

    structs chapter complete

---
+++ structs/shapes.go
@@ -0,0 +1,38 @@
+package structs
+
+import "math"
+
+// In Go interface resolution is implicit. If the type you pass in matches what the interface is asking for, it will compile
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height / 2
+}
