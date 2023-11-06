bd426365593c3b408153b3bee5078d1403fcbe0b
Frankie <frankidatank@gmail.com>
2023-11-06 16:44:55 -0700

    complete maps chapter

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -12,8 +12,11 @@ func TestWallet(t *testing.T) {
 
 	t.Run("withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
-		assertNoError(t, err)
+		err := wallet.Withdraw(Bitcoin(10))
+		if err != nil {
+			assertNoError(t, err)
+		}
+
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
